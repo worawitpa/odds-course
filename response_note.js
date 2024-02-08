fb5dcf2b43be2060ae9f604726123932b8fc94b2
Pachara Charumram <pachara.c.dev@gmail.com>
2024-02-08 16:02:22 +0700

    fix:

---
+++ response_note.js
@@ -0,0 +1,23 @@
+[{
+    "class_master_id": "C001",
+    "class_name": "Agile For Executive",
+    "class_description": "Flexibility and adaptability has became vital attribute to survive in\nmany business industries. This course is designed to share how\nAgile practices helps increase agility in business and what we\nshould consider before start an adoption.",
+    "class_picture": "https://img2.pic.in.th/pic/1000005158.webp",
+    "icon": null,
+    "create_date": null,
+    "update_date": null,
+    "update_by": null,
+    "price": "20000",
+    "picture_id": null
+}, {
+    "class_master_id": "C002",
+    "class_name": "Agile/Product Discovery",
+    "class_description": "Product Discovery is a course that blends solid product management, user\nexperience design, and design thinking into a collaborative team-based\napproach to product ownership. Product ownership is one of the most critical\nand most difficult roles in an agile process.",
+    "class_picture": "https://img2.pic.in.th/pic/1000005157ba517158ed756623.png",
+    "icon": null,
+    "create_date": null,
+    "update_date": null,
+    "update_by": null,
+    "price": "30000",
+    "picture_id": null
+}]
